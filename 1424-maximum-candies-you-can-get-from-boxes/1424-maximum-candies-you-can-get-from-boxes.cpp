39e2744b6d512bbe9a8a92cacfe8f3dc52600833
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-06-03 23:53:22 +0500

    Time: 197 ms (5.29%), Space: 69.8 MB (5.29%) - LeetHub

---
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+       
+        long long ans=0;
+        set<int> boxes,key,visited,can_open;
+
+        for(int i=0;i<status.size();i++){
+            if(status[i]==1){
+                key.insert(i);
+            }
+        }
+
+        for(int i=0;i<initialBoxes.size();i++){
+            boxes.insert(initialBoxes[i]);
+            if(key.find(initialBoxes[i])!=key.end()){
+                can_open.insert(initialBoxes[i]);
+            }
+        }
+        
+        while(!can_open.empty()){
+            int lab= *can_open.begin();
+            if(visited.find(lab)!=visited.end()){
+                can_open.erase(lab);
+                continue;
+            }
+            can_open.erase(lab);
+            visited.insert(lab);
+            ans=ans+candies[lab]*1ll;
+            for(int i=0;i<keys[lab].size();i++){
+                key.insert(keys[lab][i]);
+                if(boxes.find(keys[lab][i])!=boxes.end()){
+                    can_open.insert(keys[lab][i]);
+                }
+            }
+            for(int i=0;i<containedBoxes[lab].size();i++){
+                boxes.insert(containedBoxes[lab][i]);
+                if(status[containedBoxes[lab][i]]==1){
+                    can_open.insert(containedBoxes[lab][i]);
+                }else if(key.find(containedBoxes[lab][i])!=key.end()){
+                    can_open.insert(containedBoxes[lab][i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
