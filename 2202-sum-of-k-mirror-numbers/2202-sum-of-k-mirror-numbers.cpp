1b37e284505d7098e1fdfdf135fd25e6533aa77b
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-06-24 00:37:27 +0500

    Time: 2195 ms (18.84%), Space: 181 MB (21.74%) - LeetHub

--- 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.cpp
@@ -9,16 +9,6 @@ bool isPalindrome(const string& s) {
     return true;
 }
 
-long long stringToLongLong(const string& s) {
-    long long result = 0;
-    int i = 0;
-    while (i < s.size() && isdigit(s[i])) {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
-    return result;
-}
-
 string baseN(long long num,int base){
     string s="012345678";
     string ans="";
@@ -35,7 +25,6 @@ public:
         long long ans=0;
         int c=0;
         long long num=0;
-        // vector<string> result(1099998);
         for (int length = 1; c<n; ++length) {
             int halfLength = (length + 1) / 2;
             int start = (length == 1) ? 1 : pow(10, halfLength - 1);
@@ -46,22 +35,13 @@ public:
                 for (int j = length % 2 == 0 ? half.size() - 1 : half.size() - 2; j >= 0; --j) {
                     full += half[j];
                 }
-                //result[num]=full;
-                //num++;
-                long long temp=stringToLongLong(full);
+                long long temp=stoll(full);
                 if(isPalindrome(baseN(temp,k))){
                     c++;
                     ans=temp+ans;
                 }
             }
         }
-        //for(int i=0;c<n;i++){
-        //     long long num=stringToLongLong(result[i]);
-        //     if(isPalindrome(baseN(num,k))){
-        //         c++;
-        //         ans=ans+num;
-        //     }
-        // }
         return ans;
     }
 };
