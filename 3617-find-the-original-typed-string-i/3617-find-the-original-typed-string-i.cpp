425c6620a155362bd777f178cb5a1239c6f9b904
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-07-01 22:44:11 +0500

    Time: 0 ms (100%), Space: 9 MB (10.92%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n=word.size();
+        vector<int> con;
+        int i=0;
+        while(i<n){
+            char temp=word[i];
+            int c=0;
+            while(temp==word[i]){
+                i++;
+                c++;
+            }
+            if(c>1){
+                con.push_back(c);
+            }
+        }
+        int ans=1;
+        for(int i=0;i<con.size();i++){
+            cout<<con[i]<<"c ";
+            int temp=con[i]-1;
+            ans=ans+temp;
+            cout<<ans<<" ";
+        }
+        return ans;
+    }
+};
