34009d320ac5b5d4d1931c4f2ed35287d26386be
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-08-12 00:33:17 +0500

    Time: 19 ms (65.43%), Space: 144.9 MB (82.45%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<long long> powers;
+        int sum=0;
+        for(int i=0;i<31;i++){
+            if( n & (1ll<<i) ){
+                powers.push_back(1ll<<i);
+            }
+        }
+        vector<int> ans(queries.size());
+        for(int i=0;i<queries.size();i++){
+            long long prod=1;
+            for(int j=queries[i][0];j<=queries[i][1];j++){
+                int mod=1e9+7;
+                prod=(1ll*prod*powers[j])%(mod);
+                ans[i]=prod;
+            }
+        }
+        return ans;
+    }
+};
