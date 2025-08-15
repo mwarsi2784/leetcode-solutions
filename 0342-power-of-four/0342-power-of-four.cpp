52cc0b92b0ea3649a6bcdeb3b3a7a676096aad2a
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-08-16 00:33:50 +0500

    Time: 0 ms (100%), Space: 7.8 MB (82.39%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(!(n&(n-1))){
+            for(int i=0;i<=31;i++){
+                if(n&(1<<i)){
+                    if(i%2==0){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+};
