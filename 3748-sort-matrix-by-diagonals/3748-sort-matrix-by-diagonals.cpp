e545824cf3608fc5ecee5857e340f274734e35b0
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-08-29 23:29:49 +0500

    Time: 0 ms (100%), Space: 41.7 MB (97.66%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        for(int k=0;k<=10;k++){
+        for(int k=0;k<=9;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     if(i<n-1 && j<n-1 && i>=j){
