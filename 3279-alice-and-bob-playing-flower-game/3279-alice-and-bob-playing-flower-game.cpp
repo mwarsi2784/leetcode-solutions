89ff5b76e4d90b476bec3b85a6eb908ecbefb648
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2025-08-29 23:10:01 +0500

    Time: 0 ms (100%), Space: 8.5 MB (12.44%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -7,8 +7,8 @@ public:
         long even2=0;
         even1=n/2;
         even2=m/2;
-        odd1=((n%2)+n)/2;
-        odd2=((m%2)+m)/2;
+        odd1=(1+n)/2;
+        odd2=(1+m)/2;
         return odd2*even1+even2*odd1;
     }
 };
