9a0ece4cd3a186fb58bbfc08544edc1fd429110b
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-01-31 17:14:06 +0500

    Time: 0 ms (100%), Space: 19.7 MB (88.8%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int ind=upper_bound(letters.begin(),letters.end(),target)-letters.begin();
+        return (ind==letters.size())?letters[0]:letters[ind];
+    }
+};
