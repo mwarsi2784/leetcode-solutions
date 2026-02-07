575239fd84b811c3215dc8dc4beeeb91181a6d26
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-02-07 22:41:26 +0500

    Time: 15 ms (82.84%), Space: 25.8 MB (50.3%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
         int ans=0;
         int bCount=0;
         for(int i=0;i<s.size();i++){
