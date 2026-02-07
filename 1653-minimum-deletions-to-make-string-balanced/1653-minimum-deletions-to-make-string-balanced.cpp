78c9bb7595aed6c494166a7b46ab6f98ec0b6f7b
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-02-07 22:47:19 +0500

    Time: 8 ms (95.56%), Space: 25.5 MB (72.34%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -5,7 +5,8 @@ public:
         cin.tie(NULL);
         int ans=0;
         int bCount=0;
-        for(int i=0;i<s.size();i++){
+        int n=s.size();
+        for(int i=0;i<n;++i){
             if(s[i]=='b') bCount++;
             if(s[i]=='a'){
                 if(bCount>0){
