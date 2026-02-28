16ead33c7ebe9e09b33ac8be3b34bbced0870f97
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-01 00:33:21 +0500

    Time: 0 ms (100%), Space: 8.8 MB (14.77%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,16 +1,21 @@
+long long dp[100001]={0};
+int last=1;
 class Solution {
 public:
     int concatenatedBinary(int n) {
         int bitCount=0;
-        long long ans=0;
+        long long ans=dp[last-1];
+        if(dp[n]!=0){
+            return dp[n];
+        }
         int mod=1e9+7;
-        for(int i=1;i<=n;i++){
-            if((i&(i-1))==0){
-                bitCount++;
-            }
+        for(int i=last;i<=n;i++){
+            bitCount=32-__builtin_clz(i);
             ans<<=bitCount;
             ans=( ans | i )%mod;
+            dp[i]=ans;
         }
+        last=n;
         return ans;
     }
 };
