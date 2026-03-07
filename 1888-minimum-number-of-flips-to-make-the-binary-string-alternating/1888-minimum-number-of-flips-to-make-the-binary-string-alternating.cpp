7dc412e905e9c4bc0785fa639a574c50b842a04e
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-08 01:57:25 +0500

    Time: 4 ms (93.23%), Space: 14.3 MB (84.21%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        int n=s.size();
+        int ans[2]={0,0};
+        for(int i=0;i<n;i++){
+            ans[(s[i]^i)&1]++;
+        }
+        int res = min(ans[0],ans[1]);
+        for(int i=0;i<n;i++){
+            ans[(s[i]^i)&1]--;
+            ans[(s[i]^(n+i))&1]++;
+            res=min(res,min(ans[0],ans[1]));
+        }
+        return res;
+    }
+};
