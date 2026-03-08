d5a6e657f702a3228d9b99a773426df5a9a2f3d9
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-08 16:14:02 +0500

    Time: 19 ms (13.34%), Space: 68.3 MB (11.31%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int stringToInt(string &s){
+        int ans=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            ans= ans + ((s[i]&1)*(1<<(n-i-1)));
+        }
+        cout<<ans<<" ";
+        return ans;
+    } 
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        vector<int> freq(66000,0);
+        for(int i=0;i<nums.size();i++){
+            freq[stringToInt(nums[i])]++;
+        }
+        int n=nums.size();
+        for(int i=0;i<17;i++){
+            if(freq[i]==0){
+                string s="";
+                int num=i;
+                for(int j=0;j<n;j++){
+                    if(num%2==0){
+                        s=s+'0';
+                    }else{
+                        s=s+'1';
+                    }
+                    num=num/2;
+                }
+                reverse(s.begin(),s.end());
+                return s;
+            }
+        }
+        return "";
+    }
+};
