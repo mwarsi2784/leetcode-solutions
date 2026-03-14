d2525989edce571e8608653127dbed110c5dc886
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-15 02:57:54 +0500

    Time: 20 ms (30.17%), Space: 22 MB (43.62%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -4,7 +4,7 @@ class Solution {
             return {"a","b","c"};
         }
         vector<string> temp = fun(n-1);
-        int len = 3*pow(2,n-1);
+        int len = 3*(1<<(n-1));
         vector<string> ans(len,"");
         int j=0;
         for(char i='a';i<='c';i++){
@@ -18,7 +18,7 @@ class Solution {
     }
 public:
     string getHappyString(int n, int k) {
-        int len = 3*pow(2,n-1);
+        int len = 3*(1<<(n-1));
         if(k>len){
             return "";
         }
