cfe1adde29df2a5a37f7eae63c9ed2183218bf51
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-16 00:45:59 +0500

    Time: 85 ms (49.31%), Space: 192.4 MB (68.75%) - LeetHub

---
+++ 1622-fancy-sequence/1622-fancy-sequence.cpp
@@ -0,0 +1,53 @@
+class Fancy {
+public:
+    long long add=0;
+    long long mult=1;
+    long long mod=1e9+7;
+    vector<long long> seq;
+
+    long long pow1(long long base, long long exponent) {
+        long long result = 1;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exponent /= 2; 
+        }
+        return result;
+    }
+
+    Fancy() {
+        
+    }
+    
+    void append(int val) {
+        long long temp = ((val-add+mod)%mod) * pow1(mult,mod-2)%mod;
+        seq.push_back(temp);
+    }
+    
+    void addAll(int inc) {
+        add = (add+inc)%mod;
+    }
+    
+    void multAll(int m) {
+        mult =  (mult*m)%mod;
+        add = (add*m)%mod;
+    }
+    
+    int getIndex(int idx) {
+        if(idx>=seq.size()){
+            return -1;
+        }
+        return (seq[idx]*mult+add)%mod;
+    }
+};
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy* obj = new Fancy();
+ * obj->append(val);
+ * obj->addAll(inc);
+ * obj->multAll(m);
+ * int param_4 = obj->getIndex(idx);
+ */
