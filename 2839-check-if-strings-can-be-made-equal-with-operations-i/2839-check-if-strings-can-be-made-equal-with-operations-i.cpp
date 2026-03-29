c9e03c943b0eef8519d57652d935e7e3e3042896
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-03-29 23:20:38 +0500

    Time: 2 ms (19.35%), Space: 9.3 MB (9.03%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    bool fun(string s1,string s2){
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        if(s1==s2){
+            return true;
+        }
+        return false;
+    }
+public:
+    bool canBeEqual(string s1, string s2) {
+        string s1even=string(1,s1[0])+s1[2];
+        string s1odd=string(1,s1[1])+s1[3];
+        string s2even=string(1,s2[0])+s2[2];
+        string s2odd=string(1,s2[1])+s2[3];
+        if(fun(s1even,s2even) && fun(s2odd,s1odd)){
+            return true;
+        }
+        return false;
+    }
+};
