63963974ad5d56061f596bbecd07d8e7adb470eb
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-04-11 23:40:01 +0500

    Time: 1857 ms (5%), Space: 412.7 MB (5%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -4,8 +4,8 @@ public:
         map<int,vector<int>> f;
         int ans=1000000000;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            f[nums[i]]=vector<int>(3,1000000000);
+        for(int i=0;i<=n;i++){
+            f[i]=vector<int>(3,1000000000);
         }
         for(int i=0;i<n;i++){
             f[nums[i]][0]=f[nums[i]][1];
