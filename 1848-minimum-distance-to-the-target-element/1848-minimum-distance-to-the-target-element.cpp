c64dd145fb6ee2ae4cf61ce4352568879e6cb734
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-04-13 22:01:16 +0500

    Time: 0 ms (100%), Space: 13.2 MB (51.84%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -4,10 +4,16 @@ public:
         int ans1=1000000;
         int ans2=1000000;
         for(int i=start;i>=0;i--){
-            if(nums[i]==target) ans1=min(ans1,abs(start-i));
+            if(nums[i]==target){
+                ans1=min(ans1,abs(start-i));
+                break;
+            } 
         }
         for(int i=start;i<nums.size();i++){
-            if(nums[i]==target) ans2=min(ans2,abs(start-i));
+            if(nums[i]==target){
+                ans2=min(ans2,abs(start-i));
+                break;
+            } 
         }
         return min(ans1,ans2);
     }
