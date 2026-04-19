a975cdac5881132e58fa719f01b15aa1a22e5b8d
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-04-19 15:47:12 +0500

    Time: 3 ms (55.59%), Space: 102.2 MB (74.44%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -21,7 +21,7 @@ public:
         int ans=0;
         for(int i=0,j=0;i<n1 && j<n2;){
             if(i<=j && nums1[i]<=nums2[j]) ans=max(ans,j-i), j++;
-            else if(i<=j) i++;
+            else if(i<j) i++;
             else j++;
         }
         return ans;
