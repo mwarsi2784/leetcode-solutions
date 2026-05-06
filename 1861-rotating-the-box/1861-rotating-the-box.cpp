9e132a1f55db5c00bbb633b0e50d7ba051da79c5
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-05-06 22:45:41 +0500

    Time: 11 ms (65.85%), Space: 56.8 MB (38.33%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -3,8 +3,6 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size();
         int n = box[0].size();
-
-        // Step 1: Apply gravity
         for (int i = 0; i < m; i++) {
             int empty = n - 1;
             for (int j = n - 1; j >= 0; j--) {
@@ -17,8 +15,6 @@ public:
                 }
             }
         }
-
-        // Step 2: Rotate
         vector<vector<char>> res(n, vector<char>(m));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
