eedd468a0477835fa845c60aa378d3b5517a42b2
Muhammad Zulqarnain Warsi <127664674+mwarsi2784@users.noreply.github.com>
2026-06-21 19:38:11 +0500

    Time: 49 ms (5.38%), Space: 88.2 MB (7.22%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -3,12 +3,25 @@ public:
     int maxIceCream(vector<int>& s, int coins) {
         sort(s.begin(),s.end());
         int ans=0;
-        int n=s.size();
         int ret=0;
+        int n=s.size();
+        int mx=0;
         for(int i=0;i<n;i++){
-            if(ans+s[i]<=coins){
-                ans+=s[i];
-                ret++;
+            mx=max(mx,s[i]);
+        }
+        vector<long long> freq(mx+1,0);
+        for(int i=0;i<n;i++) freq[s[i]]++;
+        for(int i=1;i<freq.size();i++){
+            if(ans+i>coins){
+                return ret;
+            }
+            if(ans+freq[i]*i<=coins){
+                ans=ans+freq[i]*i;
+                ret=ret+freq[i];
+            }else{
+                int diff=coins-ans;
+                ret=ret+diff/i;
+                ans=ans+ret*i;
             }
         }
         return ret;
